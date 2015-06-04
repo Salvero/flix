18a3990ca4ea3de47c87f4eb67417ff41ee0591b
Salman Akram <thunder9ruby@gmail.com>
2015-06-03 21:34:21 -0400

    migrate two more fields to databases

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603001507) do
+ActiveRecord::Schema.define(version: 20150604005250) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20150603001507) do
     t.decimal  "total_gross"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.date     "released_on"
   end
 
 end
