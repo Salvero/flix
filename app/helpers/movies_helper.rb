2279155cdc7123c646de75b86d8dfc7edda25171
Salman Akram <thunder9ruby@gmail.com>
2015-06-04 20:49:32 -0400

    write movie helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,13 @@
 module MoviesHelper
+	def format_total_gross(movie)
+		# number_to_currency(movie.total_gross)
+
+		# if movie.total_gross < 400000 ---> move to movie.rb (Model)
+		if movie.flop?
+			content_tag(:strong, 'Flop Flop!')
+		else
+			number_to_currency(movie.total_gross)
+		end
+
+	end
 end
