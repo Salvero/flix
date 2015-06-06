c2eea7d0d754e020dff225f462f80f094860569a
Salman Akram <thunder9ruby@gmail.com>
2015-06-05 22:31:04 -0400

    add linking pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+	root "events#index"
   get "events" => "events#index"
+  get "events/:id" => "events#show", as: "event"
+
   get "movies" => "movies#index"
-  get "movies/:id" => "movies#show"
+  get "movies/:id" => "movies#show", as: "movie"
 
 end
